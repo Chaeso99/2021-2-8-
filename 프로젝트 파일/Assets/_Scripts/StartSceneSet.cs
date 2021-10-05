57c341dd180df48c0c620c9c715d606ad726d08b
Chaeso99 <71647858+Chaeso99@users.noreply.github.com>
2021-10-05 23:43:01 +0900

    Add files via upload

---
+++ 프로젝트 파일/Assets/_Scripts/StartSceneSet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneSet : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+
+    static void FirstLoad()
+
+    {
+
+        if (SceneManager.GetActiveScene().name.CompareTo("TitleScene") != 0)
+
+        {
+
+            SceneManager.LoadScene("TitleScene");
+
+        }
+
+    }
+
+}
